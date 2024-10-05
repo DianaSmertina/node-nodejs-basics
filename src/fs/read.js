1db7417f451d7b93b50f82485e592b39299c6a5c
ssdiana <diana.sm@changenow.io>
2024-10-06 01:02:35 +0300

    feat: implement fs tasks

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    try {
+      const contents = await fs.readFile(filePath, { encoding: 'utf8' });
+      console.log(contents);
+    } catch(error) {
+      if (error.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      }
+      console.error(error);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,35 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  const filesDir = path.join(__dirname, 'files');
+  const wrongFilenamePath = path.join(filesDir, 'wrongFilename.txt');
+  const properFilenamePath = path.join(filesDir, 'properFilename.md');
+
+  try {
+    try {
+      await fs.access(wrongFilenamePath);
+    } catch (error) {
+      throw new Error('FS operation failed');
+    }
+
+    try {
+      await fs.access(properFilenamePath);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        fs.rename(wrongFilenamePath, properFilenamePath)
+      } else {
+        console.error(error.message)
+      }
+    }
+
+  } catch (error) {
+    console.error(error.message)
+  }
 };
 
 await rename();
