3c3a44a795c99258ade2f0fff33eb832b674daab
ssdiana <diana.sm@changenow.io>
2024-10-07 22:30:12 +0300

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
+  createReadStream(filePath)
+    .pipe(createGzip())
+    .pipe(createWriteStream(archivePath))
 };
 
 await compress();
